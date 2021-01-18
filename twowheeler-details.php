410cd8e7d5d3d31217216665f65010db4be97242
sandeep45274 <70270481+sandeep45274@users.noreply.github.com>
2021-01-18 12:02:48 +1000

    Add files via upload

---
+++ twowheeler-details.php
@@ -0,0 +1,248 @@
+<?php
+session_start();
+error_reporting(0);
+
+include('includes/dbconnection.php');
+?>
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+
+<head>
+
+    <title>Carcollab - Two Wheeler Detail</title>
+
+    <!--=== Bootstrap CSS ===-->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <!--=== Slicknav CSS ===-->
+    <link href="assets/css/plugins/slicknav.min.css" rel="stylesheet">
+    <!--=== Magnific Popup CSS ===-->
+    <link href="assets/css/plugins/magnific-popup.css" rel="stylesheet">
+    <!--=== Owl Carousel CSS ===-->
+    <link href="assets/css/plugins/owl.carousel.min.css" rel="stylesheet">
+    <!--=== Gijgo CSS ===-->
+    <link href="assets/css/plugins/gijgo.css" rel="stylesheet">
+    <!--=== FontAwesome CSS ===-->
+    <link href="assets/css/font-awesome.css" rel="stylesheet">
+    <!--=== Theme Reset CSS ===-->
+    <link href="assets/css/reset.css" rel="stylesheet">
+    <!--=== Main Style CSS ===-->
+    <link href="style.css" rel="stylesheet">
+    <!--=== Responsive CSS ===-->
+    <link href="assets/css/responsive.css" rel="stylesheet">
+
+
+    <!--[if lt IE 9]>
+        <script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body class="loader-active">
+
+    <!--== Preloader Area Start ==-->
+    <div class="preloader">
+        <div class="preloader-spinner">
+            <div class="loader-content">
+                <img src="assets/img/preloader.gif" alt="JSOFT">
+            </div>
+        </div>
+    </div>
+    <!--== Preloader Area End ==-->
+
+   <?php include_once('includes/header.php');?>
+
+    <!--== Page Title Area Start ==-->
+    <section id="page-title-area" class="section-padding overlay">
+        <div class="container">
+            <div class="row">
+                <!-- Page Title Start -->
+                <div class="col-lg-12">
+                    <div class="section-title  text-center">
+                        <h2>Two Wheeler</h2>
+                        <span class="title-line"><i class="fa fa-car"></i></span>
+                        <p>Find your two wheeler to rent.</p>
+                    </div>
+                </div>
+                <!-- Page Title End -->
+            </div>
+        </div>
+    </section>
+    <!--== Page Title Area End ==-->
+
+    <!--== Car List Area Start ==-->
+    <section id="car-list-area" class="section-padding">
+        <div class="container">
+            <div class="row">
+                <!-- Sidebar Area Start -->
+                <div class="col-lg-4">
+                    <div class="sidebar-content-wrap">
+                        <!-- Single Sidebar Start -->
+                        <div class="single-sidebar">
+                            <h3>For More Informations</h3>
+<?php
+ $query=mysqli_query($con,"select * from  tblpage where PageType='contactus'");
+ while ($row=mysqli_fetch_array($query)) {
+
+
+ ?>
+                            <div class="sidebar-body">
+                                <p><i class="fa fa-mobile"></i> +<?php  echo $row['MobileNumber'];?></p>
+                                <p><i class="fa fa-clock-o"></i> Mon - Sat 8.00 - 18.00</p>
+                            </div>
+                        </div>
+                         <?php } ?>
+                        <!-- Single Sidebar End -->
+
+
+
+                        <!-- Single Sidebar Start -->
+                        <div class="single-sidebar">
+                            <h3>Connect with Us</h3>
+
+                            <div class="sidebar-body">
+                                <div class="social-icons text-center">
+                                    <a href="#" target="_blank"><i class="fa fa-facebook"></i></a>
+                                    <a href="#" target="_blank"><i class="fa fa-twitter"></i></a>
+                                    <a href="#" target="_blank"><i class="fa fa-behance"></i></a>
+                                    <a href="#" target="_blank"><i class="fa fa-linkedin"></i></a>
+                                    <a href="#" target="_blank"><i class="fa fa-dribbble"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Single Sidebar End -->
+                    </div>
+                </div>
+                <!-- Sidebar Area End -->
+
+                <!-- Car List Content Start -->
+                <div class="col-lg-8">
+                    <div class="car-list-content m-t-50">
+                        <!-- Single Car Start -->
+                        <?php
+
+//Getting default page number
+        if (isset($_GET['pageno'])) {
+            $pageno = $_GET['pageno'];
+        } else {
+            $pageno = 1;
+        }
+        // Formula for pagination
+        $no_of_records_per_page = 6;
+        $offset = ($pageno-1) * $no_of_records_per_page;
+        $total_pages_sql = "SELECT COUNT(*) FROM tblvehiclecar";
+        $ret1=mysqli_query($con,"select COUNT(*) from  tblvehicle");
+$total_rows = mysqli_fetch_array($ret1)[0];
+$total_pages = ceil($total_rows / $no_of_records_per_page);
+ $query=mysqli_query($con,"select * from tblvehicle LIMIT $offset, $no_of_records_per_page");
+ while ($row=mysqli_fetch_array($query)) {
+
+
+ ?>
+
+                        <div class="single-car-wrap">
+                      <div class="row">
+
+                                <div class="col-lg-5">
+
+                                        <img src="admin/images/<?php echo $row['Image'];?>" width="400" height='100'>
+
+                                </div>
+                                <!-- Single Car Thumbnail -->
+
+                                <!-- Single Car Info -->
+                                <div class="col-lg-7">
+
+                                    <div class="display-table">
+                                        <div class="display-table-cell">
+                                            <div class="car-list-info">
+                                                <h2><a href="#"><?php echo $row['VehicleName'];?></a></h2>
+                                                <h5><?php echo $row['RentalPrice'];?>/day</h5>
+ <p><?php echo substr($row['VehicleDescription'],0,200);?></p>
+
+                                                <p class="rating">
+                                                    <i class="fa fa-star"></i>
+                                                    <i class="fa fa-star"></i>
+                                                    <i class="fa fa-star"></i>
+                                                    <i class="fa fa-star"></i>
+                                                    <i class="fa fa-star unmark"></i>
+                                                </p>
+                                               <a href="#" class="rent-btn">More info</a>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </div>
+                                <!-- Single Car info -->
+                            </div>
+                        </div>
+                         <?php } ?>
+                        <!-- Single Car End -->
+
+
+
+                    <!-- Page Pagination Start -->
+                    <div class="page-pagi">
+                        <nav aria-label="Page navigation example">
+
+                             <ul class="pagination" >
+        <li class="page-item"><a class="page-link" href="?pageno=1"><strong>First</strong></a></li>
+        <li class="<?php if($pageno <= 1){ echo 'disabled'; } ?>">
+            <a class="page-link" href="<?php if($pageno <= 1){ echo '#'; } else { echo "?pageno=".($pageno - 1); } ?>"><strong style="padding-left: 10px">Prev</strong></a>
+        </li>
+        <li class="<?php if($pageno >= $total_pages){ echo 'disabled'; } ?>">
+            <a class="page-link" href="<?php if($pageno >= $total_pages){ echo '#'; } else { echo "?pageno=".($pageno + 1); } ?>"><strong style="padding-left: 10px">Next</strong></a>
+        </li>
+        <li class="page-item"><a class="page-link"  href="?pageno=<?php echo $total_pages; ?>"><strong style="padding-left: 10px">Last</strong></a></li>
+    </ul>
+                        </nav>
+                    </div>
+                    <!-- Page Pagination End -->
+                </div>
+                <!-- Car List Content End -->
+            </div>
+        </div>
+    </section>
+    <!--== Car List Area End ==-->
+
+   <?php include_once('includes/footer.php');?>
+
+    <!--== Scroll Top Area Start ==-->
+    <div class="scroll-top">
+        <img src="assets/img/scroll-top.png" alt="JSOFT">
+    </div>
+    <!--== Scroll Top Area End ==-->
+
+    <!--=======================Javascript============================-->
+    <!--=== Jquery Min Js ===-->
+    <script src="assets/js/jquery-3.2.1.min.js"></script>
+    <!--=== Jquery Migrate Min Js ===-->
+    <script src="assets/js/jquery-migrate.min.js"></script>
+    <!--=== Popper Min Js ===-->
+    <script src="assets/js/popper.min.js"></script>
+    <!--=== Bootstrap Min Js ===-->
+    <script src="assets/js/bootstrap.min.js"></script>
+    <!--=== Gijgo Min Js ===-->
+    <script src="assets/js/plugins/gijgo.js"></script>
+    <!--=== Vegas Min Js ===-->
+    <script src="assets/js/plugins/vegas.min.js"></script>
+    <!--=== Isotope Min Js ===-->
+    <script src="assets/js/plugins/isotope.min.js"></script>
+    <!--=== Owl Caousel Min Js ===-->
+    <script src="assets/js/plugins/owl.carousel.min.js"></script>
+    <!--=== Waypoint Min Js ===-->
+    <script src="assets/js/plugins/waypoints.min.js"></script>
+    <!--=== CounTotop Min Js ===-->
+    <script src="assets/js/plugins/counterup.min.js"></script>
+    <!--=== YtPlayer Min Js ===-->
+    <script src="assets/js/plugins/mb.YTPlayer.js"></script>
+    <!--=== Magnific Popup Min Js ===-->
+    <script src="assets/js/plugins/magnific-popup.min.js"></script>
+    <!--=== Slicknav Min Js ===-->
+    <script src="assets/js/plugins/slicknav.min.js"></script>
+
+    <!--=== Mian Js ===-->
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
